c1a2b60fc8f8d25b7cc6d7191429bd326930de97
Sondre <sondre-h@hotmail.com>
2022-11-24 14:56:36 +0100

    added buttons

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,25 +1,124 @@
 import axios from 'axios';
-import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import React, { useEffect } from 'react';
+import Highcharts from 'highcharts/highstock';
+import React, { useEffect, useState } from 'react';
+
+Highcharts.setOptions({
+	lang: {
+		months: [
+			'January',
+			'February',
+			'Mars',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		],
+		weekdays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+		rangeSelectorZoom: '',
+	},
+	accessibility: { enabled: false },
+});
 
 const LandingPage = () => {
+	const [stockOptions, setStockOptions] = useState({
+		boost: {
+			useGPUTranslations: true,
+		},
+
+		chart: {
+			backgroundColor: 'rgb(250, 250, 250)',
+			zoomType: 'x',
+		},
+
+		date: {
+			dateFormat: 'dd/mm/YYYY',
+		},
+
+		rangeSelector: {
+			buttons: [
+				{
+					type: 'year',
+					count: 1,
+					text: '1y',
+					title: 'View 1 year',
+				},
+				{
+					type: 'year',
+					count: 5,
+					text: '5y',
+					title: 'View 5 years',
+				},
+				{
+					type: 'all',
+					text: 'All',
+					title: 'View all',
+				},
+			],
+			selected: 0, //all
+		},
+
+		tooltip: {
+			valueDecimals: 2,
+		},
+
+		xAxis: {
+			title: {
+				text: 'Time',
+			},
+			type: 'datetime',
+		},
+
+		yAxis: {
+			labels: {
+				// eslint-disable-next-line no-template-curly-in-string
+				format: '${text} USD',
+			},
+			title: {
+				text: 'Stock value',
+			},
+		},
+	});
+	let stockArray = [];
+
 	useEffect(() => {
 		axios
-			.get('https://stockdata.test.quantfolio.dev/ticker/AAPL:NASDAQ')
+			.get('https://stockdata.test.quantfolio.dev/ticker')
 			.then((response) => {
-				const stockData = response.data.values;
-				const closeStockData = stockData.map((value) => parseFloat(value.close));
-				return closeStockData;
+				response.data.tickers.forEach((e) => {
+					axios
+						.get(`https://stockdata.test.quantfolio.dev/ticker/${e}`)
+						.then((response) => {
+							const stockValues = response.data.values
+								.map((values) => {
+									return [Date.parse(values.datetime), parseFloat(values.close)];
+								})
+								.reverse();
+							stockArray.push({ name: `${response.data.meta.symbol}`, data: stockValues });
+						})
+						.then(() => {
+							setStockOptions({
+								...stockOptions,
+								series: stockArray,
+							});
+						});
+				});
 			})
-			.catch((error) => console.error('Something went wrong'));
+			.catch((error) => console.error(error));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
 		<>
 			<HighchartsReact
 				highcharts={Highcharts}
-				options={closeStockData}
+				options={stockOptions}
+				constructorType={'stockChart'}
 			/>
 		</>
 	);
